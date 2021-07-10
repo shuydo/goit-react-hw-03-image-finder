6a90ad52210f668e9c8bedeecc899f6c5ab67067
Shuydo <shuydo12@gmail.com>
2021-07-10 20:38:18 +0300

    1st - input

---
+++ src/App.js
@@ -0,0 +1,16 @@
+// import "./App.css";
+import React, { Component } from "react";
+import Searchbar from "./Comps/Searchbar";
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Searchbar />
+      </div>
+    );
+  }
+}
+
+// `<Searchbar>`, `<ImageGallery>`, `<ImageGalleryItem>`,
+// `<Loader>`, `<Button>` ,`<Modal>`
